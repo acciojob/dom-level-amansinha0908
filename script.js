f97f1966687b340e1196c90d0fa10a1a7ed53f00
amansinha0908 <139490539+amansinha0908@users.noreply.github.com>
2025-09-22 10:53:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const element = document.getElementById("level");
+
+let level = 0;
+let current = element;
+
+// Traverse up through all parent nodes until reaching <html>
+while (current) {
+  level++;
+  current = current.parentElement;
+}
+
+// Show the result
+alert("The level of the element is: " + level);
